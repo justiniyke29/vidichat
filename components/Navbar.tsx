d3347f86ab70b21f717bd7c1df5c190e37ce778c
justiniyke29 <106402840+justiniyke29@users.noreply.github.com>
2023-01-10 01:36:11 +0100

    Created Discover component

---
+++ components/Navbar.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { GoogleLogin,googleLogout } from '@react-oauth/google';
+import { AiOutlineLogout } from 'react-icons/ai';
+import { BiSearch } from 'react-icons/bi';
+import { IoMdAdd } from 'react-icons/io';
+import Logo from '../utils/tiktik-logo.png';
+
+const Navbar = () => {
+  return (
+    <div className='w-full flex justify-between items-center border-b-2 border-gray-300 py-2 px-4'>
+      <Link href='/'>
+        <div className='w-[100px] md:w-[130px]' >
+            <Image
+              src={Logo}
+              className='cursor-pointer'
+              alt='vidichat logo'
+               />
+        </div>
+      </Link>
+    </div>
+  )
+}
+
+export default Navbar
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,35 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+ import type { AppProps } from 'next/app';
+ import { useState, useEffect } from 'react';
+ import Navbar from '../components/Navbar';
+ import Sidebar from '../components/Sidebar';
+ import { GoogleOAuthProvider } from '@react-oauth/google';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+ import '../styles/globals.css'
+
+ const MyApp = ({ Component, pageProps }: AppProps) => {
+  const [isSSR, setIsSSR] = useState(true);
+
+  useEffect(() =>{
+    setIsSSR(false);
+  }, []);
+
+  if(isSSR) return null;
+
+  return (
+    <div>
+      <GoogleOAuthProvider clientId = ''>
+      <Navbar />
+      <div className='flex gap-6 md:gap-20' >
+        <div className='h-[92vh] overflow-hidden xl:hover:overflow-auto'>
+          <Sidebar />
+        </div>
+          <div className='mt-4 flex flex-col gap-10 overflow-auto h-[88vh] videos flex-1 '>
+            <Component {...pageProps} />
+          </div>
+      </div>
+      </GoogleOAuthProvider>
+    </div>
+  )
 }
+
+export default MyApp
